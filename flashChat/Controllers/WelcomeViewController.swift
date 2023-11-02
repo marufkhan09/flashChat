373f7ffefed0f38b14fb569f871a9b1a90e0fcf9
marufkhan09 <maruf1609@gmail.com>
2023-11-03 02:50:19 +0600

    animation added for flash screen

--- flashChat/Controllers/WelcomeViewController.swift
+++ flashChat/Controllers/WelcomeViewController.swift
@@ -8,10 +8,24 @@
 import UIKit
 
 class WelcomeViewController: UIViewController {
-
+    @IBOutlet weak var titleLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        titleAnimation(title: "⚡️FlashChat")
+    }
+    
+    func titleAnimation(title:String){
+        var charIndex = 0.0
+        print(charIndex)
+       
+        for letter in title {
+            Timer.scheduledTimer(withTimeInterval: 0.1 * charIndex, repeats: false) { timer in
+                self.titleLabel.text?.append(String(letter))
+            }
+            charIndex += 1
+        }
     }
 
 
