bdb7fcc0b3bc79e23f9581e111fa8faa6d51eb64
marufkhan09 <maruf1609@gmail.com>
2023-11-04 03:32:23 +0600

    logout function added

--- flashChat/Controllers/ChatViewController.swift
+++ flashChat/Controllers/ChatViewController.swift
@@ -6,13 +6,27 @@
 //
 
 import UIKit
+import FirebaseAuth
+import Toast
 
 class ChatViewController: UIViewController {
-
+    let firebaseAuth = Auth.auth()
     override func viewDidLoad() {
         super.viewDidLoad()
+        navigationItem.hidesBackButton = true
+        title = "⚡️FlashChat"
 
         // Do any additional setup after loading the view.
     }
 
+    @IBAction func logoutPressed(_ sender: UIBarButtonItem) {
+        do {
+          try firebaseAuth.signOut()
+            navigationController?.popToRootViewController(animated: true)
+        } catch let e {
+            self.view.makeToast("Error signing out: %@" + e.localizedDescription)
+            
+            
+        }
+    }
 }
